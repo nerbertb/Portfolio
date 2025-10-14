498442237b9d30678a5ba60e60a7ef4cc27a4d26
nerbertb <nerbertb@gmail.com>
2025-10-15 03:00:01 +0800

    fix: homepage

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,29 @@ import About from './home/About';
 import BaseLayout from './home/layouts/base-layout';
 
 function App() {
-
-  const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <BaseLayout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: 'about', element: <About /> },
-      
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: <BaseLayout />,
+        children: [
+          { index: true, element: <Home /> },
+          { path: 'about', element: <About /> },
+          { path: 'contact', element: <Contact /> },
+        ],
+      },
     ],
-  },
-]);
-  
-  return ( 
-    <div className='bg-[url(@/assets/layered-peaks-dark-blue.svg)] w-full min-h-dvh bg-cover bg-no-repeat bg-center overflow-x-hidden'>
+    {
+      basename: import.meta.env.BASE_URL,
+    }
+  );
+
+  return (
+    <div className="bg-[url(@/assets/layered-peaks-dark-blue.svg)] w-full min-h-dvh bg-cover bg-no-repeat bg-center overflow-x-hidden">
       <LoadingScreen />
       <RouterProvider router={router} />
-      
     </div>
-   );
+  );
 }
 
 export default App;
